51683632ead04989ae68780b034c8ac211a8fe41
JoseMarin <jmarinmalagon@gmail.com>
2023-11-01 23:58:13 +0100

    basic auth

--- api_py/api/views.py
+++ api_py/api/views.py
@@ -2,12 +2,16 @@
 from rest_framework import viewsets
 from .models import Departamento, Empleado
 from .serializers import DepartamentoSerializer, EmpleadoSerializer
+from rest_framework.permissions import IsAuthenticated
+
 
 class DepartamentoList(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Departamento.objects.all()
     serializer_class = DepartamentoSerializer
 
 class EmpleadoList(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Empleado.objects.all()
     serializer_class = EmpleadoSerializer
 
