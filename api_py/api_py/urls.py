c83c182ef2388b474f8d44eca705a26af3dd9abd
JoseMarin <jmarinmalagon@gmail.com>
2023-11-01 23:41:18 +0100

    bug fixed

--- api_py/api_py/urls.py
+++ api_py/api_py/urls.py
@@ -20,6 +20,6 @@ from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.url')),
+    path('api/', include('api.urls')),
     path('docs/', include_docs_urls(title='api documentation')),
 ]
